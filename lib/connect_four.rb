8458ee78735e61cc3f2f25a9e7e882cee19a48e5
NaimiNafis <126528516+NaimiNafis@users.noreply.github.com>
2024-05-14 13:16:34 +0900

    feat: add standard 4x4 display_board test: add #display_board and test the display ouput fix: update #display_board to solve ArgumentError Remove puts and use string representation using #map. Store c_f_spec argument into a variable

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,2 +1,16 @@
 #lib/connect_four.rb
 
+red_circle = "\u{1F534}"
+blue_circle = "\u{1F535}"
+
+class Board
+  def initialize(n = 3)
+    @board = Array.new(n) { Array.new(n, " ") }
+  end
+
+  def display_board
+    # Return the string representation of the board instead of puts(direct print to console)
+    @board.map { |row| row.join(' | ') }.join("\n")
+  end
+end
+
--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -1,2 +1,15 @@
 #spec/connect_four_spec.rb
 require './lib/connect_four.rb'
+
+#puts 
+describe Board do
+  describe "#display_board" do
+    it "displays the board of n*n" do
+      board = Board.new(4)
+      expected_output = "  |   |   |  \n  |   |   |  \n  |   |   |  \n  |   |   |  "
+      expect(board.display_board).to eql(expected_output)
+    end
+  end
+end
+
+  
